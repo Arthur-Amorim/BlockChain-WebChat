1f7c26aaf4f9ddf7d446dbf126d177b74e98f3d9
LunaRenilson <renilson.c.luna@gmail.com>
2025-07-18 06:15:14 -0300

    chat iniciado

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,5 +1,6 @@
 import Navigator from "../component/Navigator"
 import enterIcon from "../assets/enter-icon.png"
+import searchIcon from '../assets/search.png'
 
 export default function Chats() {
 	return (
@@ -10,7 +11,7 @@ export default function Chats() {
 					<div className="contacts h-3/4 bg-[#FFF7D4] rounded-4xl shadow-2xl">
 						<p className={titlesStyle}>Contacts</p>
 					</div>
-					<div className="newContact h-1/4 bg-[#FFF7D4] rounded-2xl shadow-2xl flex flex-col items-center">
+					<div className="newContact h-1/4 bg-[#FFF7D4] rounded-2xl shadow-2xl flex flex-col items-center gap-y-2">
 						<p className={titlesStyle}>Reach more friends</p>
 						<div className="flex justify-center items-end h-1/4 mt-3 gap-2">
 							<input
@@ -18,19 +19,27 @@ export default function Chats() {
 								placeholder="Hash Id"
 								className="w-5/6 h-full mt-3 border-1 rounded-md p-2 border-amber-900"
 							/>
-							<button className="w-10 bg-amber-500 p-1 h-full rounded-md hover:cursor-pointer">
+							<button className="w-10 bg-amber-300 p-1 h-full rounded-md hover:cursor-pointer">
 								<img
-									src={enterIcon}
+									src={searchIcon}
 									alt="icone de enter"
 									className="w-full"
 								/>
 							</button>
 						</div>
-						<p>Usuário encontrado?</p>
+						<p className="">Found user?</p>
 					</div>
 				</div>
-				<div className="chat h-full bg-[#FFF7D4] w-3/4 rounded-2xl shadow-2xl">
+				<div className="chat h-full bg-[#FFF7D4] w-3/4 rounded-2xl shadow-2xl relative flex flex-col">
+
 					<p className={titlesStyle}>BlockChain Talking</p>
+					<div className="chat-messages flex-grow mb-[60px]">
+
+					</div>
+					<div className="chat-input w-full absolute bottom-0 left-0 right-0 flex p-2 rounded-b-2xl h-[80px] bg-[#FFD95A] items-center">
+						<input type="text" className="rounded-full px-7 text-[16px] h-[50px] w-full border bg-white relative" placeholder="Type your message..."/>
+						<button className="w-8 absolute right-5"><img src={enterIcon} alt="enter icon" className="w-full" /></button>
+					</div>
 				</div>
 			</div>
 		</div>
